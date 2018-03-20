071a48338a05d131b3a0c0408d024168fb3fdb04
demony <yhc8245865@126.com>
2018-03-20 11:35:52 +0800

    o

--- app/src/main/java/com/witcher/testcamera2/L.java
+++ app/src/main/java/com/witcher/testcamera2/L.java
@@ -1,6 +1,8 @@
 package com.witcher.testcamera2;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by witcher on 2018/3/16 0016.
@@ -9,5 +11,8 @@ import android.util.Log;
 public class L {
     public static void i(String content){
         Log.i("witcher",content);
+    }
+    public static void t(Context ctx,String content){
+        Toast.makeText(ctx,content,Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/witcher/testcamera2/MainActivity.java
+++ app/src/main/java/com/witcher/testcamera2/MainActivity.java
@@ -4,9 +4,12 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -17,6 +20,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int REQUEST_PERMISSIONS = 888;
@@ -25,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mBtClose;
     private Button mBtSwitch;
     private SurfaceView mSvPreview;
+
+    private CameraDevice mCameraDevice;
+    private CameraCaptureSession mCameraCaptureSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,9 +75,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void switchCamera() {
-        CameraManager cameraMgr = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        if (cameraMgr != null) {
-        }
+        L.t(this,"todo");
     }
 
     private void open() {
@@ -77,22 +83,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             try {
                 CameraManager cameraMgr = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
                 if (cameraMgr != null) {
-                    cameraMgr.openCamera(""+CameraCharacteristics.LENS_FACING_BACK,stateCallback, null);
+                    cameraMgr.openCamera("" + CameraCharacteristics.LENS_FACING_BACK, stateCallback, null);
                 }
             } catch (CameraAccessException e) {
                 e.printStackTrace();
-            } catch (SecurityException e){
+            } catch (SecurityException e) {
                 e.printStackTrace();
             }
-        }else{
+        } else {
             //camera1
         }
     }
+
+    private void startPerview() {
+        if (mCameraDevice == null) {
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                mCameraDevice.createCaptureSession(Arrays.asList(mSvPreview.getHolder().getSurface()), new CameraCaptureSession.StateCallback() {
+                    @Override
+                    public void onConfigured(@NonNull CameraCaptureSession session) {
+                        mCameraCaptureSession = session;
+                        sendRepeatPreviewRequest();
+                    }
+
+                    @Override
+                    public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+                    }
+                }, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendRepeatPreviewRequest() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                CaptureRequest.Builder builder;
+                builder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                builder.addTarget(mSvPreview.getHolder().getSurface());
+                builder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+//                builder.setTag(RequestTag.Preview);
+//                addBaselineCaptureKeysToRequest(builder);
+                mCameraCaptureSession.setRepeatingRequest(builder.build(),
+                        new CameraCaptureSession.CaptureCallback() {
+                            @Override
+                            public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+                                super.onCaptureStarted(session, request, timestamp, frameNumber);
+                            }
+                        },
+                        null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(@NonNull CameraDevice camera) {
             L.i("onOpened");
+            mCameraDevice = camera;
+            startPerview();
         }
 
         @Override
@@ -107,13 +163,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     };
 
     private void closeCamera() {
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if(mCameraDevice!=null){
+                mCameraDevice.close();
+            }
+        }
     }
 
     private void openCamera() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA,}, 1);
-        }else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA,}, REQUEST_PERMISSIONS);
+        } else {
             open();
         }
     }
@@ -122,10 +182,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_PERMISSIONS) {
-            if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 open();
-            }else{
-                Toast.makeText(this,"没权限",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "没权限", Toast.LENGTH_SHORT).show();
             }
         }
     }
